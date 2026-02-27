2d94c8f0194dd05c4fcd26ef58a65c8faae37478
Raballah <rabspro@gmail.com>
2026-02-27 13:33:59 +0300

    Swapping Variables through Destructuring

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,8 @@
-// A Rust program to determine the min and max numbers out of a given array list.
-// Ages of girls in a community study meeting: [23, 31, 18, 21, 22, 12, 28, 29, 27, 32, 17, 16, 20, 26]
-
-fn age_categorized(girl_age: &[i32]) -> (i32, i32) {
-    let mut min_age = girl_age[0];
-    let mut max_age = girl_age[0];
-
-    for &age in girl_age {
-        if age < min_age {
-            min_age = age;
-        }
-        if age > max_age {
-            max_age = age;
-        }
-    }
-
-    (min_age, max_age)
-}
-
+//let's now focus on swapping variables through destructuring 
+// Rust makes it possible to swap two variables elegantly using tuple destructuring
 fn main() {
-    let girl_age = [23, 31, 18, 21, 22, 12, 28, 29, 27, 32, 17, 16, 20, 26];
-    let (min_age, max_age) = age_categorized(&girl_age);
+    let person = ("Alice", 39, false);
+    let (name, age, is_active) = person;
 
-    println!("The youngest girl is aged {}, while the oldest girl has an age of {}.", min_age, max_age);
+    println!("{} used to be {} years old when you were 9 years old. is this right?: {}", name, age, is_active);
 }
