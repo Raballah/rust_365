fcee75d0f4c7d64c080392f06c76fde466049dcd
Raballah <rabspro@gmail.com>
2026-03-13 13:37:13 +0300

    Day 4, Task 4 N

--- task4_project/src/main.rs
+++ task4_project/src/main.rs
@@ -1,20 +1,54 @@
-// Day 4: Lessons Regarding Control Flow (if, else, else if, match
-// The match, but operates the same way as if statements. match as expresssion, or as a variable.
-// The match feature in Rust
+// Day 4: Lessons Regarding Control Flow (if, else, else if, match)
+// Student Grade Evaluator Mini Task
 
 fn main() {
-    let weight = 34;
 
-    let weight_category = match weight {
-        w if w > 100 => "obese",
-        90..=100 => "very overweight",
-        80..=89 => "fat",
-        70..=79 => "normal",
-        60..=69 => "slim",
-        50..=59 => "thin",
-        40..=49 => "emaciated",
-        _ => "frail",
+    // if else
+    let score = 78;
+
+    if score >= 90 && score <=100 {
+        println!("Grade: A");
+    } else if score >=80 && score <=89 {
+        println!("Grade: B");
+    } else if score >=70 && score <=79 {
+        println!("Grade: C");
+    } else if score >=60 && score <=69 {
+        println!("Grade: D");
+    } else if score < 60 {
+        println!("Grade: F");
+    } else {
+        println!("Grade: Unknown");
+    }
+
+    let pass_status = if score >= 60 {
+        "Passed"
+    } else {
+        "Failed"
     };
 
-    println!("Based on his weight readings now, he is definitely {}.", weight_category);
+    println!("\n --Based on the score, this studet has {}.--", pass_status);
+
+
+    // match 
+
+    let mark = 78; 
+
+    let grade = match mark {
+        w if w > 100 => "Grade: Unknown",
+        90..=100 => "Grade: A",
+        80..=89 => "Grade: B",
+        70..=79 => "Grade: C",
+        60..=69 => "Grade: D",
+        w if w < 60 => "Grade: F",
+        _ => "Grade: Uncategorized",
+    };
+
+    let pass_determination = if mark >= 60 {
+        "Passed"
+    } else {
+        "Failed"
+    };
+
+    println!("\n The grade of this student is {}.", grade);
+    println!("\n --The mark indicates that this student has {}.--", pass_determination);
 }
