4623cd9f5fbc3c51d601ea7d16648830dbb8ce7b
Raballah <rabspro@gmail.com>
2026-03-14 13:02:34 +0300

    Day 5, Task B

--- day5_project/src/main.rs
+++ day5_project/src/main.rs
@@ -1,19 +1,17 @@
 // Day 5: Loops 
-// loop keyword. loops forevoer until you stop it, is it using break or continue?
-// loop is infinite loop. runs forever, until you stop it.
-// loop keyword is a loop that counts forever, untily stopped. infinite loop. 
-// stop using break
+// Loops that return values, more Rust-like
+// Count my money, break to return value, and then subtract to get balance.
 
 fn main() {
-    let mut hundreds = 100;
+    let mut money_counter = 0;
 
-    loop {
-        hundreds += 100;
+    let my_balance = loop {
+        money_counter += 1;
 
-        println!("This is part of the hundreds: {}", hundreds);
-        
-        if hundreds == 700 {
-            break;
+        if money_counter == 1200 {
+            break money_counter - 122;
         }
-    }
+    };
+
+    println!("My balance from the money availabe is {}", my_balance);
 }
