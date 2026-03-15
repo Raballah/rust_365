c0f392d61aae3ef99b770b48921589cef2947421
Raballah <rabspro@gmail.com>
2026-03-15 11:35:55 +0300

    Day 5, Task E

--- day5_project/src/main.rs
+++ day5_project/src/main.rs
@@ -1,22 +1,28 @@
 // Day 5: Loops 
 // While loop
-// Simple command-line tool. Reading from a buffer.
+// Building a multi-line text buffer 
 
 use std::io;
 
 fn main() {
-    let mut user_input = String::new();
+    let mut document = String::new();
+    let mut line = String::new();
 
-    while user_input.trim() != "exit" {
-        user_input.clear(); // Clear existing, previous entry
-        println!("Type something to proceed or 'exit' to quit!");
+    println!("Type 'done' to exit!:");
+
+    loop {
+        line.clear();
 
         io::stdin()
-            .read_line(&mut user_input)
-            .expect("Failed to read any input from the line!");
+            .read_line(&mut line)
+            .expect("Failed to read line");
         
-        println!("Your Entry: {}", user_input);
+        if line.trim() == "done" {
+            break;
+        }
+
+        document.push_str(&line);
     }
 
-    println!("Exited successfully!");
+    println!("Document progress: \n{}", document);
 }
