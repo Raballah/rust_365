61c5169b91e3b610c89ecd5b63903d5b625abef0
Raballah <rabspro@gmail.com>
2026-03-17 09:52:20 +0300

    Day 5, Task J

--- day5_project/src/main.rs
+++ day5_project/src/main.rs
@@ -1,15 +1,15 @@
 // Day 5: Loops 
-// for loop. Using for with ranges.
-// rust iterators using continue feature.
+// for loop, mutable and immutable borrows.
 
 fn main() {
-    let numbers = vec![1,2,3];
+    let mut numbers = vec![5, 10, 15];
 
-    for n in numbers.iter() { // creates an interator, which yields &i32
-        println!("{}", n);  // println! macro implements Display trait, automatically handles conversion of &i32 to i32 
+    for n in numbers.iter() {
+        println!("{}", n);
     }
 
-    for n in numbers {  // Implements numbers.into_iter() interation, consumption iteration. n is i32 here, within Vec<i32)
-        println!("{}", n);  // prints, but moves ownership of numbers vector to the loop iterator. numbers vector unavaiable beyond this for loop scope.
+    for n in numbers.iter_mut() {
+        *n += 5;
     }
+    println!("{:?}", numbers);
 }
