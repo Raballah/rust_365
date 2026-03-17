059c47932bcc0db10a1803471f290445e88e7c87
Raballah <rabspro@gmail.com>
2026-03-17 12:38:34 +0300

    Day 5, Task M

--- day5_project/src/main.rs
+++ day5_project/src/main.rs
@@ -4,13 +4,10 @@
 fn main() {
     let mut numbers = vec![1, 2, 3];
 
-    for n in &mut numbers {  // mutable borrow of the vector (read and write), yields an iterator n with the pointer: &mut i3
-        *n += 1;  // *n deferences the pointer, allows access to the referenced i32 elements of the collection/vector
-    } // borrow ends here. vector modified to vec![2, 3, 4] (no printing here.)
-
-    for n in &numbers { // borrow vector, an iteractor n with the pointer: &i32
-        println!("{}", n); // println! macro implements Display. 2, 3, and 4 printed.  
-    } // borrow ends here. vector remains vec![2, 3, 4], available for use beyond this for loop iterator scope.
-
-    println!("{:?}", numbers); // prints [2, 3, 4]
+    let first = &numbers[0];
+    println!("{}", first);
+    
+    for n in &mut numbers {
+        *n += 1;
+    }
 }
