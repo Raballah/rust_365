a298b0671102e3a28f9fa2fd932e565cdcaec75e
Raballah <rabspro@gmail.com>
2026-03-17 11:06:58 +0300

    Day 5, Task L

--- day5_project/src/main.rs
+++ day5_project/src/main.rs
@@ -4,15 +4,13 @@
 fn main() {
     let mut numbers = vec![1, 2, 3];
 
-    for n in &numbers {  // Borrows the vector, produces an iteractor n: &132
-        println!("{}", n); // Prints 1, 2, 3. println! macro implements Display
-    } // borrow ends, vector released as vec![1, 2, 3]
+    for n in &mut numbers {  // mutable borrow of the vector (read and write), yields an iterator n with the pointer: &mut i3
+        *n += 1;  // *n deferences the pointer, allows access to the referenced i32 elements of the collection/vector
+    } // borrow ends here. vector modified to vec![2, 3, 4] (no printing here.)
 
-    for n in &mut numbers {  // mutable borrow of the vector, we have iteractor n: &mut i32
-        *n *=2;  // * deferences the pointer, giving the iteractor pointer &mut i32, access to the i32 elements.
-    } // borrow ends here, but yields/modifies initial vector to vec![2, 4, 6]
+    for n in &numbers { // borrow vector, an iteractor n with the pointer: &i32
+        println!("{}", n); // println! macro implements Display. 2, 3, and 4 printed.  
+    } // borrow ends here. vector remains vec![2, 3, 4], available for use beyond this for loop iterator scope.
 
-    for n in numbers { // moves ownership of the vector to the for iteractor loop
-        println!("{}", n); // prints 2, 4, and 6, the i32 elements as borrowed.
-    } // borrowing ends here. ownership moves to the for loop iterator. vector not available for further use beyond this beyond.
+    println!("{:?}", numbers); // prints [2, 3, 4]
 }
