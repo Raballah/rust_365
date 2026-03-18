21d5039df51274549d1f48d1b2610ca5e7f0c054
Raballah <rabspro@gmail.com>
2026-03-18 10:52:42 +0300

    Day 5, Task P

--- day5_project/src/main.rs
+++ day5_project/src/main.rs
@@ -1,21 +1,27 @@
 // Day 5: Loops 
 // Looop Labels. Rust allows loops to be labelled, calld labelled loops. 
+// Case Example: Searching Through 2D Dataset
+// Say I want to search for the number 17 in an excel sheet dataset, within a range.
 
 fn main() {
-    let mut _count = 0;
+    let rope_length = [
+        [78, 128, 24, 18],
+        [89, 133, 28, 90],
+        [109, 56, 48, 46],
+        [110, 72, 83, 71],
+        [107, 94, 32, 21],
+    ];
 
-    'outer: loop {
-        println!("Outer loop!");
+    let target = 28;
 
-        loop {
-            println!("Inner loop");
-            
-            loop {
-                println!("Will this work, and why?");
+    'outer: for row in 0..5 {
+        for col in 0..4 {
+            if rope_length[row] [col] == 28 {
+                println!("Found {} at ({}, {}).", target, col, row);
                 break 'outer;
             }
         }
     }
 
-    println!("Done looping for now!");
+    println!("Search completed successfully!");
 }
