fe15262e8d4e90d102b90615c75db7999495f36d
Raballah <rabspro@gmail.com>
2026-03-19 11:08:28 +0300

    Day 5, Task S

--- day5_project/src/main.rs
+++ day5_project/src/main.rs
@@ -1,30 +1,15 @@
-// Day 5: Loops 
-// Looop Labels. Rust allows loops to be labelled, calld labelled loops. 
-// Day 5 Mini Project: Student Attendance Simulation
-// The project entails simulating students arrriving at a school 
-
-/*
-Student attendance simulation. Simulating students entering a class. Program has a total of 
-10 students. Uses a loop to simulate attendance counting. each loop increases the number of students present. Stop when the classroom is full.
-Print attendance progress.  Constraints: program must use loop, break, for, while, continue
-you many split them into sections in the same program. add this rule. if student number 5 arrives, skip printing them using continue.
-so, expected output excludes 5. 
-Will this suggested code print. offer corrections based on this approach. 
-*/
+// Day 6: Functions and Return Values
+// Day 6 Mini Project: "Student Score Analyzer CLI"
 
 fn main() {
-    let total_students = 10;
-    let mut present = 0;
+    let number_given = 15;
 
-    for student in 1..=total_students {
-        if student == 5 {
-            println!("Student 5 not counted.");
-            continue;
+    match number_given {
+        w if w < 1 => println!("Ignore these!"),
+        1 => println!("Lowest Number!"),
+        2..=13 => println!("Almost a Big Number!"),
+        worst_nightmare @ 14..17 => println!("This number is my {}", worst_nightmare),
+        17..=20 => println!("Largest of them all."),
+        _ => println!("Not necessary!"),
         }
-
-        present += 1;
-        println!("Student {} Entered. Currently Present: {}", student, present);
-    }
-
-    println!("Final Attendance: {}/{}", present, total_students);
 }
