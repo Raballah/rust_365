3c799830290bbc908fa8379ce89c95d0f65a9223
Raballah <rabspro@gmail.com>
2026-03-20 15:53:41 +0300

    Day 5, Task W

--- day5_project/src/main.rs
+++ day5_project/src/main.rs
@@ -3,34 +3,81 @@
 // Building a simple CLI program, which takes a student's score.
 // uses functions to determine the grade.
 
-fn score_grader(score: i32) -> char {
+// 1. Grade Calculator
+
+fn calculate_grade(score: i32) -> char {
     match score {
-        w if w > 100 => 'E',
+        w if w > 100 || w < 0 => 'I', // 'I' invalid score entered. reccheck entry.
         80..=100 => 'A',
         70..=79 => 'B',
         60..=69 => 'C',
         50..=59 => 'D',
-        40..=49 => 'E',
-        _ => 'F',
+        0..=49 => 'F',
+        _ => 'I',
     }
 }
 
-fn pass_determinant(mark: i32) -> &'static str {
-    if mark >= 50 {
-        "Pass"
-    } else {
-        "Fail"
+// 2. Pass/Fail Checker
+
+fn is_pass(score: i32) -> bool {  // is_pass is Verbose bool return.
+    score >= 50
+}
+
+// 3. Feedback Generator
+
+fn feedback(score: i32) -> String {
+    match score {
+        w if w > 100 || w < 0 => String::from("Invalid score: Recheck score entry!"),
+        80..=100 => String::from("Excellent work!"),
+        70..=79 => String::from("Almost there. Keep working harder!"),
+        60..=69 => String::from("Good job. Keep improving!"),
+        50..=59 => String::from("Fair trial. Keep improving!"),
+        0..=49 => String::from("Needs improvement!"),
+        _ => String::from("Invalid score: Recheck score entry!"),
     }
+}
+
+// 4. Return tuple
+
+fn analyze(score: i32) -> (char, bool, String) {
+    (calculate_grade(score), is_pass(score), feedback(score))
 }
 
 fn main() {
-    let student_score = 43;
+    let score = 109;
 
-    let grade = score_grader(student_score);
-    let pass_level = pass_determinant(student_score);
+    let grade = calculate_grade(score);
+    let passed = is_pass(score);
+    let message = feedback(score);
 
     println!("\n--- Students' Grading and Pass Categorization---");
 
-    print!("Grade: {}", grade);
-    println!(" | Pass Level: {}", pass_level);
+    println!("Grade: {} | Pass?: {} | Feedback: {}", grade, passed, message);
+
+    let score_analysis = analyze(score);
+
+    println!("\n--- Score analysis as a tuple---");
+
+    println!("Score analysis: {:?}", score_analysis);
+}
+
+// Not sure of how to acheive this requirement: "Accept user input from terminal". An opportunity to learn here.
+
+// Some lessons
+
+.contains(&score) takes integer slice.
+
+pub fn contains(&self, item: &i32) -> bool
+
+(0..=100).contains(&score)
+
+fn is_valid(score: i32) -> bool {
+    (0..=100).contains(&score)
+}
+
+fn is_pass(score: i32) -> Option<bool> {
+    if !(0..100).contains(&score) {
+        return None;  // Invalid - no pass/fail assigned
+    }
+    Some(score >= 50) // valid - evaluate normally.
 }
