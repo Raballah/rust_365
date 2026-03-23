8494ebba8ac1f7be9831f03a1731e5f427e083f1
Raballah <rabspro@gmail.com>
2026-03-23 17:12:14 +0300

    Day 6, Task A

---
+++ day6_project/src/main.rs
@@ -0,0 +1,11 @@
+// Day 7 - Mini Project 1
+// CLI Student Management Tool
+// A real Rust CLI application combinng functions, loops, match/control flow, user input, data handling
+// Goal: Build an interactive CLI tool, which accepts multiple student scores
+
+
+fn main() {
+    let score = 72;
+
+    println!("The student's score is {}.", score);
+}
