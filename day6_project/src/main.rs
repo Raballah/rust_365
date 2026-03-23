bd08260184be9c426cb44ca8da00cf0d3c06d666
Raballah <rabspro@gmail.com>
2026-03-23 17:41:48 +0300

    Day 6, Task B

--- day6_project/src/main.rs
+++ day6_project/src/main.rs
@@ -3,9 +3,43 @@
 // A real Rust CLI application combinng functions, loops, match/control flow, user input, data handling
 // Goal: Build an interactive CLI tool, which accepts multiple student scores
 
+use std::io;
+
+fn is_valid(score: i32) -> bool {
+    (0..=100).contains(&score)
+}
 
 fn main() {
-    let score = 72;
+    loop {
+        let mut input = String::new();
 
-    println!("The student's score is {}.", score);
+        println!("Enter Student Score or 'exit' to Exit: ");
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+        
+        let small_case = input.trim();
+        let trimmed = small_case.to_lowercase();
+
+        if trimmed == "exit" {
+            println!("Session Exited Successfully!");
+            break;
+        }
+
+        let score: i32 = match trimmed.parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid Entry. Score Must be From 0-100.");
+                continue;
+            }
+        };
+
+        if !is_valid(score) {
+            println!("Invalid Entry. Score Must be From 0-100.");
+            continue;
+        }
+
+        println!("Student's Score: {}", score);
+    }
 }
