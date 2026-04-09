e0b58c16b0cc0383638bb52246cbf13e7e6bf177
Raballah <rabspro@gmail.com>
2026-04-09 15:02:12 +0300

    approached the non-lexical lifetimes NLL in borrowing

--- day8_project/src/main.rs
+++ day8_project/src/main.rs
@@ -1,11 +1,52 @@
 // Day 9, borrowing and references
+// Day 9 Mini Project - Borrowing Fix 
 
-fn change_name(naming: &mut String) {
-    naming.push_str(" Smart");
+fn football(game: &str) {
+    println!("Some hate {}, but it's still popular globally", game);
 }
-fn main() {
-    let mut name = String::from("James");
-    change_name(&mut name);
 
-    println!("Full Names: {}", name);
+fn to_owned_string(game: &str) -> String {
+    game.to_string()  // ensures the &str is converted to the expected String outcome.
+}
+
+fn add_suffix(text: &mut String) {
+    text.push_str(" is fun.");
+}
+
+fn prepend_text(game: &mut String) {
+    game.insert_str(0, "We love ");
+}
+
+fn main() {
+    let mut game = String::from("soccer");
+    let favourite_game = &game; // borrows 'soccer' no ownership of value taken here.
+
+    println!("Favourite game: {}", favourite_game); // favourite_game borrows game as str, &soccer, println! macro implements Display trait to soccer 
+    
+    let hated_game = &game;  // borrowed as str, we have &soccer here
+
+    football(hated_game); // fn deferences &soccer to soccer. &soccer still available because of borrow
+
+    println!("Are you sure {} is a hated game?", hated_game); //works just fine, no ownership taken due to &str borrow
+
+    // Returning ownership 
+
+    let game_giver = to_owned_string(&game); // here, favourite_game is still &str, that is &soccer, the value. no ownership taken by function.
+
+    println!("{} is now available.", game_giver); // println! automatically deferences the borrow to display soccer. no ownership taken by println! macro
+
+    // addiing a string to game:
+    add_suffix(&mut game);
+
+    println!("Personal view on soccer: {}", game);
+
+    // I'm assuming that beyond this scope/ point, favourite_game and hated_game are unaivalable / 
+    // cannot be used further because game has been modified. Is this correct? or since we have &mut game, they still valid?
+
+    // now, say I wanted add some text before the word 'soccer' after game has been modified in this scope. 
+    // would this work? I know of the rule of not having more than 1 &mut String concurrently, 
+    // but how would I go about this? or is there a way to build on an already modified string?
+    
+    prepend_text(&mut game);
+    println!("{}", game);
 }
