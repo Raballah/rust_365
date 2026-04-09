a04e71889361028b85575ea703127ac7f6687512
Raballah <rabspro@gmail.com>
2026-04-09 11:32:01 +0300

    Returned ownership

--- day8_project/src/main.rs
+++ day8_project/src/main.rs
@@ -6,10 +6,22 @@ fn football(game: String) {
     println!("Some hate {}, but it's still popular globally", game);
 }
 
+// Return ownership
+fn give_back(game: String) -> String {
+    game
+}
+
 fn main() {
     let game = String::from("soccer");
     let favourite_game = game;
+
+    // println!("I like {}.", game); error because value borrowed after move.
+
     let hated_game = favourite_game.clone();
     
     football(hated_game);
+
+    let game_giver = give_back(favourite_game);
+
+    println!("{} is now available!", game_giver);
 }
