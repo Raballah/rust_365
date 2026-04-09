b800d8663a5882205b6c7629d21c7b12586b0b44
Raballah <rabspro@gmail.com>
2026-04-09 11:17:07 +0300

    Day 8, files

--- day8_project/src/main.rs
+++ day8_project/src/main.rs
@@ -1,3 +1,15 @@
+// Day 8 - Ownership
+// mini project - Onwership playground
+// A small program to demonstrate move, clone, ownership transfer via function
+
+fn football(game: String) {
+    println!("Some hate {}, but it's still popular globally", game);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let game = String::from("soccer");
+    let favourite_game = game;
+    let hated_game = favourite_game.clone();
+    
+    football(hated_game);
 }
