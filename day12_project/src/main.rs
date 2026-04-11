49d73770b2fc2684243cee7c1b7049240d3fa166
Raballah <rabspro@gmail.com>
2026-04-11 11:58:53 +0300

    Day 12 Project

--- day12_project/src/main.rs
+++ day12_project/src/main.rs
@@ -1,3 +1,20 @@
+struct Student {
+        name: String,
+        score: i32,
+        passed: bool,
+    }
+
 fn main() {
-    println!("Hello, world!");
+    let mut student = Student {
+        name: String::from("Alice"),
+        score: 85,
+        passed: true,
+    };
+
+    student.name = String::from("Mercy");
+    student.passed = false;
+
+    println!("{}", student.name);
+    println!("{}", student.score);
+    println!("{}", student.passed);
 }
