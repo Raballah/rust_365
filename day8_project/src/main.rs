4a0a7644f3710f80f86bd6f363dbaf82adace52b
Raballah <rabspro@gmail.com>
2026-04-15 16:18:59 +0300

    Added code with struct formulas

--- day8_project/src/main.rs
+++ day8_project/src/main.rs
@@ -3,6 +3,7 @@
 
 // Display first, 4th, and last word in this sentence
 
+/*
 fn main() {
     let sentence = String::from("You will not believe what I just found out!");
 
@@ -22,3 +23,51 @@ fn main() {
     let fourth = sentence.split_whitespace().nth(3).unwrap_or("none");
     println!("Fourth word: {}", fourth);
 } 
+*/
+struct FruitBox {
+    customer_name: String,
+    fruit_type: String,
+    children: u32,
+}
+
+impl FruitBox {
+    // calculating shipping cost based on fruit type
+    fn calculate_shipping(&self) -> f64 {
+        if self.fruit_type == "Mango" {
+            10.50 // mangos are heavy
+        } else {
+            5.00 // others are lighter/cheaper to ship.
+        }
+    }
+}
+
+fn main() {
+    let mut major_package: Vec<FruitBox> = vec![
+        FruitBox { customer_name: String::from("John"), fruit_type: String::from("Mango"), children: 4 },
+        FruitBox { customer_name: String::from("Mary"), fruit_type: String::from("Oranges"), children: 3 },
+        FruitBox { customer_name: String::from("Moses"), fruit_type: String::from("Apples"), children: 1 },
+        FruitBox { customer_name: String::from("Kay"), fruit_type: String::from("Mango"), children: 4 },
+        FruitBox { customer_name: String::from("Paul"), fruit_type: String::from("Oranges"), children: 6 },
+    ];
+
+    // Display item in one of the structs
+    println!("Mary's box contains {}", major_package[1].fruit_type);
+
+    if let Some(marys_box)  = major_package.iter_mut().find(|b| b.customer_name == "Mary") {
+        marys_box.children += 1;        
+        println!("Updated: {} now has {} children.", marys_box.customer_name, marys_box.children);
+    } else {
+        println!("No box found for Mary!");
+    } 
+
+    let children_fed: u32 = major_package.iter().map(|s| s.children).sum();
+    println!("Total children enjoying our fruits: {}", children_fed);
+
+
+
+    let total_cost: f64 = major_package
+        .iter()
+        .map(|box_item| box_item.calculate_shipping())
+        .sum();
+    println!("The total cost of shippping is ${:.1}", total_cost);
+}
