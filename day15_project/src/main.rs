1ad7f76145364bb339f3e6c52759ef6b517c1309
Raballah <rabspro@gmail.com>
2026-04-26 13:34:30 +0300

    accessing values in vectors .get method

--- day15_project/src/main.rs
+++ day15_project/src/main.rs
@@ -2,15 +2,20 @@
 // Accessing elements in vectors, indexing can panic. use .get()
 
 fn main() {
-    let numbers = vec![1, 3, 7];
+    let numbers = vec![11, 34, 74, 12, 78, 37, 57];
 
-    match numbers.get(1) {
+    match numbers.get(5) {
+        Some(value) => println!("Value at index 5: {}", value),
+        None => println!("No value at index 5!"),
+    }
+
+    match numbers.get(20) {
         Some(val) => println!("Value of x: {}", val),
         None => println!("Such index not found!"),
     }
 
-    if let Some(val) = numbers.get(4) {
-        println!("Value at index 4: {}", val);
+    if let Some(num) = numbers.get(4) {
+        println!("Value at index 4: {}", num);
     } else {
         println!("The vector has non value at index 4!");
     }
