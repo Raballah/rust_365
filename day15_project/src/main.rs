7e5d5f640b47e44de52c51a1da07981c88cf245f
Raballah <rabspro@gmail.com>
2026-04-26 13:24:19 +0300

    Adjusted day 15

--- day15_project/src/main.rs
+++ day15_project/src/main.rs
@@ -1,3 +1,17 @@
+// on Vectors and Collections
+// Accessing elements in vectors, indexing can panic. use .get()
+
 fn main() {
-    println!("Hello, world!");
+    let numbers = vec![1, 3, 7];
+
+    match numbers.get(1) {
+        Some(val) => println!("Value of x: {}", val),
+        None => println!("Such index not found!"),
+    }
+
+    if let Some(val) = numbers.get(4) {
+        println!("Value at index 4: {}", val);
+    } else {
+        println!("The vector has non value at index 4!");
+    }
 }
