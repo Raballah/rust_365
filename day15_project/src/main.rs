67521eaf3224f87e693ddd30f069a925a7c0fa9b
Raballah <rabspro@gmail.com>
2026-04-27 15:02:15 +0300

    added a check for numbers between 0 and 100

--- day15_project/src/main.rs
+++ day15_project/src/main.rs
@@ -14,18 +14,32 @@ fn read_input(prompt: &str) -> String {
     input.trim().to_string()
 }
 
+fn menu_display() {
+    println!("\n=== Score Tracker ===");
+
+        println!("\n1. Add score");
+        println!("2. View all scores");
+}
+
 struct Mark {
     score: i32,
 }
 
 impl Mark {
-    fn new(score: i32) -> Self {
-        Self { score }
+    fn new(score: i32) -> Option<Self> {
+        if (0..=100).contains(&score) {
+            Some( Self { score })
+        } else {
+            None
+        }
     }
 }
 
 // Student score entry app
-
+// let mut scores: Vec<Mark> = Vec::new();
+// loop {
+// show
+//}
 struct App {
     scores: Vec<Mark>,
 }
@@ -38,7 +52,12 @@ impl App {
     }
 
     fn run(&mut self) {
+        // add student
         self.add_score();
+
+        // show main menu
+        menu_display();
+
     }
 
     fn add_score(&mut self) {
@@ -52,11 +71,28 @@ impl App {
 
             let score: i32 = match trimmed.parse::<i32>() {
                 Ok(num) => num,
-                Err(_) =>{
-                    println!("Invalid entry. Enter numbers from 0 to 100");
+                Err(_) => {
+                    println!("Invalid entry. Score must be a number, from 0 to 100");
                     continue;
                 }
-            }; 
+            };
+
+            match Mark::new(score) {
+                Some(mark) => {
+                    self.scores.push(mark);
+                    println!("Score added!");
+                },
+                None => println!("Invalid. Enter non-negative numbers, from 0 to 100!"),
+            }
+            // Last score added and total scores added so far.
+            match self.scores.last() {
+                Some(last_mark) => println!(
+                    "Last score added: {} Total scores: {}", 
+                    last_mark.score, 
+                    self.scores.len()
+                ),
+                None => println!("No score added so far!"),
+            }
         }
     }
 }
