a4649b728b6e01edd917d2562153b49f26e93ee4
Raballah <rabspro@gmail.com>
2026-05-13 21:54:24 +0300

    added the use of format! for creating a String

--- day1_project/src/main.rs
+++ day1_project/src/main.rs
@@ -9,10 +9,9 @@
 }*/
 
 fn main() {
-    let small = String::from("Cow");
-    let big = String::from("Shit");
+    let first = String::from("Come");
+    let second = String::from("on!");
 
-    let bad_word = format!("{} {}", small, big);
-    println!("That is some {} talk.", bad_word);
-    println!("Small: {}, Big: {}", small, big);
+    let joint = format!("{} {}", first, second);
+    println!(" {} We are going now.", joint);
 }
