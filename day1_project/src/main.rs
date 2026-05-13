0b781bfabd7edef72c4bccd248c3078390bdff8b
Raballah <rabspro@gmail.com>
2026-05-13 13:37:43 +0300

    &str lesson on Strings in Depth

--- day1_project/src/main.rs
+++ day1_project/src/main.rs
@@ -1,3 +1,8 @@
+// Day 16 - Strings in Depth 
 fn main() {
-    println!("Hello, world!");
+    // string slice
+    let greeting: &str = "Strings in Depth";
+    println!("This is a session about {}", greeting);
+    // &str features, String slice: immutable, borrowable, fixed-length,
+    // stored on binary
 }
