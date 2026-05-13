bb78c97081e3f9f0d9d413a318b90961bd746210
Raballah <rabspro@gmail.com>
2026-05-13 14:28:40 +0300

    Strings, with implementation of Display trait {}

--- day1_project/src/main.rs
+++ day1_project/src/main.rs
@@ -1,8 +1,7 @@
 // Day 16 - Strings in Depth 
 fn main() {
-    // string slice
-    let greeting: &str = "Strings in Depth";
-    println!("This is a session about {}", greeting);
-    // &str features, String slice: immutable, borrowable, fixed-length,
-    // stored on binary
+    // Strings => owned, growable UTF-8 strings stored on the heap
+    let mut response: String = String::from("Are you sure ");
+    response.push_str("?");
+    println!("{} Yes, I am sure!", response); // Unlike vectors Vec, String implements Display trait {} for UTF-8 text
 }
