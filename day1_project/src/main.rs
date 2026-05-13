ff42e112b039c7d5e13ae5d691087c2aec39989a
Raballah <rabspro@gmail.com>
2026-05-13 21:36:31 +0300

    use of format! for contenation

--- day1_project/src/main.rs
+++ day1_project/src/main.rs
@@ -1,9 +1,18 @@
 // Day 16 - Strings in Depth 
 // the format! approach
-fn main() {
+/*fn main() {
     let small = String::from("Cow");
     let big = String::from("Shit");
 
     let bad_word = small + &big;
     println!("That is some {} talk.", bad_word);
+}*/
+
+fn main() {
+    let small = String::from("Cow");
+    let big = String::from("Shit");
+
+    let bad_word = format!("{} {}", small, big);
+    println!("That is some {} talk.", bad_word);
+    println!("Small: {}, Big: {}", small, big);
 }
