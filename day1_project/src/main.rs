58b3a53f4c3b5515e77948c3071343a31cd3a610
Raballah <rabspro@gmail.com>
2026-05-16 22:50:39 +0300

    split on space with .split(' ')

--- day1_project/src/main.rs
+++ day1_project/src/main.rs
@@ -184,11 +184,21 @@ fn main() {
 }*/
 
 // .split() uses char, space or &str to split, what is supplied is the backbone of the split
+// spliting using char character
+/*fn main() {
+    let gone = "He, 48, and, Bob, Move, 89";
+
+    for word in gone.split(',') {
+        println!("{}", word);
+    }
+}*/
+
+// Splitting using space or white space. 
 
 fn main() {
     let gone = "He, 48, and, Bob, Move, 89";
 
-    for word in gone.split(',') {
+    for word in gone.split(' ') {
         println!("{}", word);
     }
 }
